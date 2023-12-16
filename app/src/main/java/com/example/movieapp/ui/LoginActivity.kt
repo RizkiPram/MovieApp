98f17a020762eed75cb4068fbd45400c0765b174
LAPTOP-KUQT5LQP\Rizki <pramudyar7@gmail.com>
2023-12-16 18:02:21 +0700

    layout now playing movie

---
+++ app/src/main/java/com/example/movieapp/ui/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.movieapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.movieapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+    }
+}
